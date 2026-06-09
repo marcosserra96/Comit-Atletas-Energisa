64fa729fc65097ca74de36c29f4e8db4b8ba0c92
marcosserra96 <marcospauloserra@outlook.com.br>
2026-06-09 08:42:15 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,12 +1,38 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+
 import { 
-  getFirestore, collection, getDocs, doc, getDoc, setDoc, updateDoc, addDoc, deleteDoc,
-  query, where, orderBy, writeBatch, increment
+  getFirestore,
+  collection,
+  getDocs,
+  doc,
+  getDoc,
+  setDoc,
+  updateDoc,
+  addDoc,
+  deleteDoc,
+  query,
+  where,
+  orderBy,
+  writeBatch,
+  increment
 } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+
 import { 
-  getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut,
-  updateEmail, updatePassword, reauthenticateWithCredential, EmailAuthProvider
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  updateEmail,
+  updatePassword,
+  reauthenticateWithCredential,
+  EmailAuthProvider
 } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+
+import {
+  getFunctions,
+  httpsCallable
+} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-functions.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC2l8LU3vYfQjTly8JSa658mfIlVk2Dw8E",
@@ -18,13 +44,53 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
 const db = getFirestore(app);
 const auth = getAuth(app);
 
+/*
+  Cloud Functions
+
+  Se você publicou suas Functions na região padrão do Firebase,
+  normalmente é "us-central1", então pode deixar assim.
+*/
+const functions = getFunctions(app);
+
+/*
+  Se você publicou em São Paulo, use esta linha no lugar da linha acima:
+
+  const functions = getFunctions(app, "southamerica-east1");
+*/
+
 export { 
-  app, db, auth, 
-  collection, getDocs, doc, getDoc, setDoc, updateDoc, addDoc, deleteDoc,
-  query, where, orderBy, writeBatch, increment,
-  signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut,
-  updateEmail, updatePassword, reauthenticateWithCredential, EmailAuthProvider
+  app,
+  db,
+  auth,
+  functions,
+
+  collection,
+  getDocs,
+  doc,
+  getDoc,
+  setDoc,
+  updateDoc,
+  addDoc,
+  deleteDoc,
+  query,
+  where,
+  orderBy,
+  writeBatch,
+  increment,
+
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  updateEmail,
+  updatePassword,
+  reauthenticateWithCredential,
+  EmailAuthProvider,
+
+  getFunctions,
+  httpsCallable
 };
